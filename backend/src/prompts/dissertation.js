262cebb8a751e4b6a4a39e93df8555109a10faf3
Code-Guess <id63302010@gmail.com>
2026-06-06 18:45:32 +0100

    Create dissertation.js

---
+++ backend/src/prompts/dissertation.js
@@ -0,0 +1,5 @@
+// src/prompts/dissertation.js
+
+const DISSERTATION_PROMPT = `[PROMPT DISSERTATION] Aide à la dissertation française. Plan dialectique ou thématique selon le sujet. Introduction avec accroche, thèse, antithèse, synthèse. Argumentation rigoureuse.`;
+
+module.exports = { DISSERTATION_PROMPT };
