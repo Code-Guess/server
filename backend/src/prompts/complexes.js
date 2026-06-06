1a64c1f18acce9d5e637c8cb7e11f7e2385b43bb
Code-Guess <id63302010@gmail.com>
2026-06-06 18:40:33 +0100

    Create complexes.js

---
+++ backend/src/prompts/complexes.js
@@ -0,0 +1,5 @@
+// src/prompts/complexes.js
+
+const COMPLEXES_PROMPT = `[PROMPT COMPLEXES] Nombres complexes z=a+ib. Module |z|, argument arg(z), conjugué z̄, forme trigonométrique et exponentielle. Formule de Moivre.`;
+
+module.exports = { COMPLEXES_PROMPT };
