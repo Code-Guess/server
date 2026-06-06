b2de66c04b50111c844bbef26e827a58a2ae9f13
Code-Guess <id63302010@gmail.com>
2026-06-06 18:41:43 +0100

    Create mathIratEq.js

---
+++ backend/src/prompts/mathIratEq.js
@@ -0,0 +1,5 @@
+// src/prompts/mathIratEq.js
+
+const MATH_IRAT_EQ_PROMPT = `[PROMPT ÉQUATIONS IRRATIONNELLES] Pour √f(x) = g(x) : conditions d'existence g(x) ≥ 0, mise au carré, vérification des solutions.`;
+
+module.exports = { MATH_IRAT_EQ_PROMPT };
