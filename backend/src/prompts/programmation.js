8f88106141121424f5f60939b3a5f292b1a1555c
Code-Guess <id63302010@gmail.com>
2026-06-06 18:46:17 +0100

    Create programmation.js

---
+++ backend/src/prompts/programmation.js
@@ -0,0 +1,5 @@
+// src/prompts/programmation.js
+
+const PROGRAMMATION_PROMPT = `[PROMPT PROGRAMMATION] Expert développement. Code complet, commenté, fonctionnel. Pas de TODO ni placeholder. Gestion des erreurs. Suit les bonnes pratiques du langage.`;
+
+module.exports = { PROGRAMMATION_PROMPT };
